3177ad79b07bc88c92de0d9f1a9a9a15df4124c1
jpt1729 <john.tan-aristy@csedge.org>
2024-09-06 10:25:22 -0500

    cache

--- src/app/[id]/route.js
+++ src/app/[id]/route.js
@@ -1,18 +1,29 @@
 import { redirect, notFound } from "next/navigation";
+
+import { cache } from 'react'; 
+
 import Airtable from "airtable";
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
   "appd18nRei0sIXoka"
 );
+
+const getURL = async (id) => {
+  records = await base('urls')
+      .select({
+        filterByFormula: `{name} = "${id}"`, // Adjust field name as needed
+      })
+      .firstPage();
+  return records
+}
+
+const cachedURL = cache(getURL)
+
 export async function GET(request, params) {
   const id = params.params.id;
   let records;
   try {
     // Search for a specific row by the "Name" field
-    records = await base('urls')
-      .select({
-        filterByFormula: `{name} = "${id}"`, // Adjust field name as needed
-      })
-      .firstPage();
+    records = await cachedURL(id)
 
     if (records.length === 0) {
       return notFound();
